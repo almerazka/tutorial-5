3d2918934083fc167e50deff41266fa0d8e388ec
almerazka <almer.azka12@gmail.com>
2026-02-27 10:59:27 +0700

    Format Player.gd

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 @export var jump_speed = -350
 @export var max_jumps = 2
 
-# DASH 
+# DASH
 @export var dash_speed = 800
 @export var dash_duration = 0.2
 @export var double_tap_time = 0.3
@@ -30,59 +30,64 @@ var is_near_ladder = false
 var is_on_rope = false
 var is_near_rope = false
 
+
 func _physics_process(delta):
 	# RESTART
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().reload_current_scene()
-		
+
 	# ROPE
 	if is_near_rope and not is_on_rope and not is_on_floor():
 		if velocity.y > 0:
 			is_on_rope = true
-		
+
 	if is_on_rope:
 		velocity.y = 0
-		
+
 		if Input.is_action_pressed("ui_left"):
 			velocity.x = -walk_speed
 		elif Input.is_action_pressed("ui_right"):
 			velocity.x = walk_speed
 		else:
 			velocity.x = 0
-		
+
 		if Input.is_action_just_pressed("ui_accept"):
 			is_on_rope = false
 			velocity.y = jump_speed
 		elif Input.is_action_pressed("ui_down"):
 			is_on_rope = false
 			velocity.y = 100
-		
+
 		move_and_slide()
 		update_animation()
 		return
-			
+
 	# LADDER
 	if is_near_ladder and not is_on_ladder:
 		if Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down"):
 			is_on_ladder = true
-		
+
 	if is_on_ladder:
 		print("ON LADDER, velocity:", velocity, "on_floor:", is_on_floor())
 		velocity = Vector2.ZERO
-		
+
 		if Input.is_action_pressed("ui_up"):
 			velocity.y = -walk_speed
 		elif Input.is_action_pressed("ui_down"):
 			velocity.y = walk_speed
-	
+
 		move_and_slide()
-		
-		if is_on_floor() and not Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_down"):
+
+		if (
+			is_on_floor()
+			and not Input.is_action_pressed("ui_up")
+			and not Input.is_action_pressed("ui_down")
+		):
 			is_on_ladder = false
-			
+
 		update_animation()
 		return
-		
+
 	# CROUCH
 	is_crouching = Input.is_action_pressed("ui_down") and is_on_floor()
 
@@ -93,10 +98,7 @@ func _physics_process(delta):
 		jump_count = 0
 
 	# JUMP
-	if Input.is_action_just_pressed("ui_accept") \
-	and jump_count < max_jumps \
-	and not is_crouching:
-		
+	if Input.is_action_just_pressed("ui_accept") and jump_count < max_jumps and not is_crouching:
 		velocity.y = jump_speed
 		jump_count += 1
 
@@ -104,7 +106,7 @@ func _physics_process(delta):
 	if is_dashing:
 		dash_timer -= delta
 		velocity.x = dash_direction * dash_speed
-		
+
 		if dash_timer <= 0:
 			is_dashing = false
 	else:
@@ -112,15 +114,16 @@ func _physics_process(delta):
 		handle_dash_input()
 
 	update_animation()
-	
+
 	var screen_width = get_viewport_rect().size.x
 	position.x = clamp(position.x, 0, screen_width)
 	move_and_slide()
 
+
 # NORMAL MOVEMENT
 func handle_movement():
 	var current_speed = walk_speed
-	
+
 	if is_crouching:
 		current_speed = crouch_speed
 
@@ -130,6 +133,7 @@ func handle_movement():
 		velocity.x = current_speed
 	else:
 		velocity.x = 0
+
 
 # DASH INPUT
 func handle_dash_input():
@@ -145,31 +149,32 @@ func handle_dash_input():
 			start_dash(1)
 		last_right_tap = current_time
 
+
 func start_dash(direction):
 	if is_crouching:
 		return
-		
+
 	is_dashing = true
 	dash_timer = dash_duration
 	dash_direction = direction
 
+
 # ANIMATION
 func update_animation():
-
 	# Flip kiri kanan
 	if velocity.x != 0:
 		$AnimatedSprite2D.flip_h = velocity.x < 0
-	
+
 	if is_on_rope:
 		play_anim("Hang")
 		return
-		
+
 	# Climb
 	if is_on_ladder:
 		if velocity.y != 0:
 			play_anim("Climb")
 		return
-		
+
 	# Dash / Slide
 	if is_dashing:
 		play_anim("Slide")
@@ -196,16 +201,19 @@ func update_animation():
 	else:
 		play_anim("Idle")
 
+
 # PLAY FUNCTION
 func play_anim(name):
 	if $AnimatedSprite2D.animation != name:
 		$AnimatedSprite2D.play(name)
+
 
 # LADDER SIGNAL
 func _on_ladder_body_entered(body):
 	print("ENTER:", body.name)
 	if body == self:
 		is_near_ladder = true
+
 
 func _on_ladder_body_exited(body):
 	print("EXIT:", body.name)
@@ -217,6 +225,7 @@ func _on_ladder_body_exited(body):
 func _on_rope_body_entered(body):
 	if body == self:
 		is_near_rope = true
+
 
 func _on_rope_body_exited(body):
 	if body == self:
